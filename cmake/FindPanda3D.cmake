eb1adeac7454b7eaccb3e967ee19532c81264ea0
jokla <giovanni.claudio@inria.fr>
2015-10-19 15:31:46 +0200

    First commit

---
+++ cmake/FindPanda3D.cmake
@@ -0,0 +1,58 @@
+#------------------------------------------------------------------------------
+# CMake Module for finding Panda3D
+#------------------------------------------------------------------------------
+
+set(PythonInterp_FIND_VERSION 2.7)
+set(PythonLibs_FIND_VERSION 2.7)
+
+# We need Python's headers
+find_package(PythonLibs REQUIRED)
+
+# Find the path of the Panda includes
+find_path(PANDA3D_INCLUDE_DIR pandaFramework.h
+    /usr/local/include/panda3d
+    /usr/include/panda3d
+	/Developer/Panda3D/include
+)
+
+# Add our stuff to the build system
+include_directories(${PANDA3D_INCLUDE_DIR} ${PYTHON_INCLUDE_DIR})
+
+set(PANDA3D_LIBS_TO_CHECK
+	p3framework
+	panda
+	p3dtool
+	p3dtoolconfig
+        p3pystub
+	p3direct
+	pandaexpress
+	pandafx
+	pandaphysics
+	pandaegg
+	p3vision
+	p3glstuff
+	pandaskel
+	p3openal_audio
+	p3tinydisplay
+	p3ptloader
+	pandaai
+	pandagl
+)
+
+foreach(_lib ${PANDA3D_LIBS_TO_CHECK})
+	find_library(P3D_${_lib}
+		NAMES
+			${_lib}
+		PATHS
+			/usr/local/lib/panda3d
+			/usr/lib/panda3d
+			/Developer/Panda3D/lib
+                        /usr/lib/x86_64-linux-gnu/panda3d/
+	)
+
+	if(P3D_${_lib})
+             message(STATUS "P3D_${_lib}")
+		add_library(Panda3D::${_lib} SHARED IMPORTED)
+		set_target_properties(Panda3D::${_lib} PROPERTIES IMPORTED_LOCATION ${P3D_${_lib}} )
+	endif(P3D_${_lib})
+endforeach(_lib ${PANDA3D_LIBS_TO_CHECK})
---
+++ p3util/cOnscreenText.h
@@ -0,0 +1,95 @@
+/*
+ * cOnscreenText.h
+ *
+ *  Created on: 2012-05-28
+ *      Author: dri
+ *
+ * COnscreenText module: contains a C++ implementation of the
+ *                       OnscreenText Python class
+ *
+ */
+
+#ifndef CONSCREENTEXT_H_
+#define CONSCREENTEXT_H_
+
+#define Colorf LColorf
+
+#include "pandaFramework.h"
+
+class COnscreenText : public NodePath
+   {
+   public:
+
+   // These are the styles of text we might commonly see.  They set the
+   // overall appearance of the text according to one of a number of
+   // pre-canned styles.  You can further customize the appearance of the
+   // text by specifying individual parameters as well.
+   enum TextStyle
+      {
+      TS_plain = 1,
+      TS_screen_title,
+      TS_screen_prompt,
+      TS_name_confirm,
+      TS_black_on_white
+      };
+
+   COnscreenText(const string& name, TextStyle style = TS_plain);
+   COnscreenText(const COnscreenText& other);
+   ~COnscreenText();
+
+   COnscreenText& operator=(const COnscreenText& other);
+
+   void cleanup();
+
+   void set_decal(bool decal);
+   bool get_decal() const;
+
+   void set_font(TextFont* font);
+   TextFont* get_font() const;
+
+   void clear_text();
+   void set_text(const string& text);
+   void append_text(const string& text);
+   string get_text() const;
+
+   void set_x(float x);
+   void set_y(float y);
+   void set_pos(float x, float y);
+   void set_pos(const LVecBase2f& pos);
+   const LVecBase2f& get_pos() const;
+
+   void set_roll(float roll);
+   float get_roll() const;
+
+   void set_scale(float scale);
+   void set_scale(float scaleX, float scaleY);
+   void set_scale(const LVecBase2f& scale);
+   const LVecBase2f& get_scale() const;
+
+   void set_wordwrap(float wordwarp);
+   float get_wordwrap() const;
+
+   void set_fg(const Colorf& fg);
+   void set_bg(const Colorf& bg);
+   void set_shadow(const Colorf& shadow);
+   void set_shadow_offset(const LVecBase2f& offset);
+   void set_frame(const Colorf& frame);
+
+   void set_align(TextNode::Alignment align);
+   void set_draw_order(int drawOrder);
+
+   NodePath generate() const;
+
+   private:
+
+   void swap(COnscreenText& other);
+   void duplicate_parenting(const COnscreenText& other);
+
+   static const float MARGIN;
+   static const float SHADOW;
+
+   struct TextNodeProxy;
+   PT(TextNodeProxy) m_textNode;
+   };
+
+#endif /* CONSCREENTEXT_H_ */
